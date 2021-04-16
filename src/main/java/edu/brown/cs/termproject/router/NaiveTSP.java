9a24fb046e4d50d744276a4a429764abece93f74
Tianren Dong <tianrendong@tianrensmacbook.lan>
2021-04-15 21:56:14 -0700

    No more checkstyle errors, Test coverage at 70%

--- src/main/java/edu/brown/cs/termproject/router/NaiveTSP.java
+++ src/main/java/edu/brown/cs/termproject/router/NaiveTSP.java
@@ -9,7 +9,11 @@ import edu.brown.cs.termproject.graph.Graph;
 import edu.brown.cs.termproject.graph.Vertex;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Approximation of TSP through MST.
